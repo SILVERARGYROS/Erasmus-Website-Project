09bd398f8736cc2f1142003175e0fa5b6352a088
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2023-06-14 19:22:33 +0300

    Database Design
    - Created database in XAMPP
    
    - Added sign-up.php file that enlists new users into the database. It also creates a local file to store each new user's uploads in.

---
+++ scripts/sign-up.php
@@ -0,0 +1,49 @@
+<?php
+
+//connecting to database
+$con = mysqli_connect("localhost:3306","root","");
+if (!$con){
+    echo "problem in the connection".mysqli_error();
+    die;
+}
+mysqli_select_db($con, "ErasmApp");
+
+
+// Form data
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$am = $_POST['am'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Generating upload folder and profile pic   
+mkdir("../uploads/$username", 0777, true);
+echo copy("../uploads/!general!/default.png", "../uploads/$username/default.png");
+$pfp = "../uploads/$username/profile_pic.png";
+
+// Adding user to database
+$result = mysqli_query($con, "INSERT INTO USERS (fname, lname, am, email, username, password, pfp) 
+                              VALUES ('$fname', '$lname', '$am', '$email', '$username', '$password', '$pfp')");
+
+// Relating user to group
+$result = mysqli_query($con, "SELECT id
+                              FROM USERS
+                              WHERE am = '$am'");
+
+$user_id = mysqli_fetch_assoc($result)['id'];
+
+
+$result = mysqli_query($con, "SELECT id
+                              FROM USER_GROUPS
+                              WHERE group_type = 'registered user'");
+
+$group_id = mysqli_fetch_assoc($result)['id'];
+
+$result = mysqli_query($con, "INSERT INTO GROUPED (user_id, group_id) 
+                              VALUES ($user_id, $group_id)");
+
+mysqli_close($con);
+
+header("location: ../index.html");
+?>

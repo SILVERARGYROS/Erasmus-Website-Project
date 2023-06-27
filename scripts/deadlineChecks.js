83e14d22ab4afecc54c22b32cc899d6e0bea0eff
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2023-06-27 17:31:43 +0300

    Deadline Page
    - Added deadline page for admin users.
    
    - Added frontend checks to validate date period.
    
    - Made form title purple in the rest of the form pages.

---
+++ scripts/deadlineChecks.js
@@ -0,0 +1,52 @@
+// Global variables
+var date_startBool = 0;
+var date_endBool = 0;
+
+function date_startCheck(){
+    var filled = document.getElementById("date_start").value.length;
+    if(filled){
+        date_startBool = 1;
+    }
+    else{
+        date_startBool = 0;
+    }
+}
+
+function date_endCheck(){
+    var filled = document.getElementById("date_end").value.length;
+    if(filled){
+        date_endBool = 1;
+    }
+    else{
+        date_endBool = 0;
+    }
+}
+
+function formCheck(){
+    startDate = document.getElementById("date_start").value;
+    endDate = document.getElementById("date_end").value;
+
+    var valid_period = new Date(startDate).getTime() < new Date(endDate).getTime();
+    document.getElementById("submit").value = valid_period; // For testing purposes
+
+    if(!valid_period){
+        document.getElementById("date_endSpan").innerHTML = "&nbsp; Invalid expiration date.";
+    }
+    else{
+        document.getElementById("date_endSpan").innerHTML = "";
+    }
+
+
+    if(date_startBool == 0 || date_endBool == 0 || !valid_period){
+        document.getElementById("submit").disabled = true;
+        document.getElementById("submit").classList.remove("form-button");
+        document.getElementById("submit").classList.add("form-button-disabled");
+    }
+    else{
+            document.getElementById("date_endSpan").innerHTML = "";
+    
+            document.getElementById("submit").disabled = false;
+            document.getElementById("submit").classList.remove("form-button-disabled");
+            document.getElementById("submit").classList.add("form-button");
+    }
+}
